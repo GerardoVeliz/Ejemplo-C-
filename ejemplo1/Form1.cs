6d094ba4ff2336b50f5d823f65cdff1eda71338d
Gerardo Veliz <gerardo.veliz@alumnos.frgp.utn.edu.ar>
2023-03-19 18:51:52 -0300

    Modificacion del cod

--- ejemplo1/Form1.cs
+++ ejemplo1/Form1.cs
@@ -16,5 +16,10 @@ namespace ejemplo1
         {
             InitializeComponent();
         }
+        //hola
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
